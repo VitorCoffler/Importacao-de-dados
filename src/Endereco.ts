77d7c87512991def45d584ed5c2aa21ac4b03450
VitorCoffler <vitorcoffler145@gmail.com>
2026-06-13 02:00:41 -0300

    criei a class Endereco

---
+++ src/Endereco.ts
@@ -0,0 +1,63 @@
+class Endereco {
+    private cep: string;
+    private logradouro: string;
+    private bairro: string;
+    private estado: string;
+    private ddd: string;
+
+    constructor(cep: string, logradouro: string, bairro: string, estado: string, ddd: string) {
+        this.cep = cep;
+        this.logradouro = logradouro;
+        this.bairro = bairro;
+        this.estado = estado;
+        this.ddd = ddd;
+    }
+
+    public get Cep(): string {
+        return this.cep;
+    }
+
+    public get Logradouro(): string {
+        return this.logradouro;
+    }
+
+    public get Bairro(): string {
+        return this.bairro;
+    }
+
+    public get Estado(): string {
+        return this.estado;
+    }
+
+    public get Ddd(): string {
+        return this.ddd;
+    }
+
+    public set Cep(cep: string) {
+        this.cep = cep;
+    }
+
+    public set Logradouro(logradouro: string) {
+        this.logradouro = logradouro;
+    }
+
+    public set Bairro(bairro: string) {
+        this.bairro = bairro;
+    }
+
+    public set Estado(estado: string) {
+        this.estado = estado;
+    }
+
+    public set Ddd(ddd: string) {
+        this.ddd = ddd;
+    }
+
+    public toString(): string {
+        return("CEP: " + this.cep + "\n" +
+               "Logradouro: " + this.logradouro + "\n" +
+               "Bairro: " + this.bairro + "\n" +
+               "Estado: " + this.estado + "\n" +
+               "DDD: " + this.ddd);
+    }
+}
